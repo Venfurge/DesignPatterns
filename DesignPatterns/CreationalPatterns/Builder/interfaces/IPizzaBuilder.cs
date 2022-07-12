88c54e49f53aa69b5191907572445b3b1da90f3d
Zhenia <59691258+Venfurge@users.noreply.github.com>
2022-07-12 19:22:50 +0300

    Builder (#15)
    * Builder pattern implemented
    
    * Update README.md

---
+++ DesignPatterns/CreationalPatterns/Builder/interfaces/IPizzaBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace Builder.interfaces
+{
+    internal interface IPizzaBuilder
+    {
+        public void Reset();
+        public void SetName(string Name);
+        public void SetDescription(string Description);
+        public void AddMozzarella();
+        public void AddPepperoni();
+        public void AddPineapple();
+        public void AddTomatoSauce();
+        public void AddCezarioSauce();
+        public void AddSalad();
+    }
+}
