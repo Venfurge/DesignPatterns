a3f1dd594030ae65ad53c549e11d2c499297ee48
Venfurge <zhek.ba@gmail.com>
2022-07-12 22:06:06 +0300

    Prototype implemented with c# basic object method

---
+++ DesignPatterns/CreationalPatterns/Prototype/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Prototype.models;
+
+Pizza onePizza = new Pizza("Cheese", "A lot of mozzarella cheese", 3.99);
+onePizza.Print();
+
+Console.WriteLine("Cloned Pizza");
+
+Pizza clonedPizza = onePizza.ShallowCopy();
+clonedPizza.Print();
