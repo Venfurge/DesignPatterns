cf8bf83cc5a85796078c3eac7f9333571729a649
Zhenia <59691258+Venfurge@users.noreply.github.com>
2022-07-13 18:58:12 +0300

    Flyweight (#18)
    * Flyweight pattern implemented
    
    * Update README.md

---
+++ DesignPatterns/StructuralPatterns/Flyweight/models/FlyweightFactory.cs
@@ -0,0 +1,61 @@
+﻿using Flyweight.interfaces;
+
+namespace Flyweight.models
+{
+    internal class FlyweightFactory
+    {
+        private List<KeyValuePair<string, Flyweight>> _flyweights = new List<KeyValuePair<string, Flyweight>>();
+
+        public FlyweightFactory(List<IPizza> pizzas)
+        {
+            pizzas.ForEach(pizza => 
+                _flyweights.Add(
+                    new KeyValuePair<string, Flyweight>(
+                        getKey(pizza), new Flyweight(pizza)
+                    )
+                ));
+        }
+
+        public string getKey(IPizza pizza)
+        {
+            List<string> elements = new List<string>();
+
+            elements.Add(pizza.ImageUrl);
+            elements.Add(pizza.Price.ToString());
+
+            if (pizza.Name != null && pizza.Description != null)
+            {
+                elements.Add(pizza.Name);
+                elements.Add(pizza.Description);
+            }
+
+            elements.Sort();
+
+            return String.Join("_", elements);
+        }
+
+        public Flyweight GetFlyweight(IPizza sharedState)
+        {
+            string key = getKey(sharedState);
+
+            if (_flyweights.Where(f => f.Key == key).Count() == 0)
+            {
+                Console.WriteLine("Log: FlyweightFactory can't find a flyweight. Creating new one");
+                _flyweights.Add(new KeyValuePair<string, Flyweight>(key, new Flyweight(sharedState)));
+            }
+            else
+            {
+                Console.WriteLine("Log: FlyweightFactory finds flyweight. Reusing existing flyweight.");
+            }
+
+            return _flyweights.Where(f => f.Key == key).FirstOrDefault().Value;
+        }
+
+        public void PrintFlyweights()
+        {
+            var count = _flyweights.Count;
+            Console.WriteLine($"FlyweightFactory: There are {count} flyweights");
+            _flyweights.ForEach(f => f.Value.Print());
+        }
+    }
+}
---
+++ DesignPatterns/StructuralPatterns/Flyweight/models/Pizza/PepperoniPizza.cs
@@ -0,0 +1,16 @@
+﻿using Flyweight.interfaces;
+
+namespace Flyweight.models.Pizza
+{
+    internal class PepperoniPizza : IPizza
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string MeatType { get; set; }
+
+        public PepperoniPizza(double price, string name, string description, string imageUrl, string meatType)
+            => (Price, Name, Description, ImageUrl, MeatType) = (price, name, description, imageUrl, meatType);
+    }
+}
